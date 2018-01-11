6388df48ce9d9be50eb387054c3cc11a6c2af9f4
dhivyavsb <31505815+dhivyavsb@users.noreply.github.com>
2018-01-11 16:53:26 +0530

    Create constant.c

---
+++ constant.c
@@ -0,0 +1,28 @@
+# include<stdio.h>
+main()
+{
+char letter;
+scanf("%c",&letter);
+printf("enter the letter");
+switch(letter)
+case a:
+printf("letter is vowel");
+break;
+case e:
+printf("letter is vowel");
+break;
+case i:
+printf("letter is vowel");
+break;
+case o:
+printf("letter is vowel");
+break;
+case u:
+printf("letter is vowel");
+break;
+default:
+printf("letter is not a vowel");
+break;
+}
+
+                                                                 
