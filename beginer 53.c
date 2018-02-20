54068f72b62e5993e11f0fd6e6b6389b44d8b1f6
dhivyavsb <31505815+dhivyavsb@users.noreply.github.com>
2018-02-20 10:54:09 +0000

    Create beginer 53.c

---
+++ beginer 53.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i,sum=0;
+for(i=1;i<10;i++)
+scanf("%d",&a[10]);
+for(i=1;i<10;i++)
+{
+printf("%d\n",a[10]);
+}
+for(i=1;i<10;i++)
+{
+sum=sum+a[j];
+}
+printf("%d\n",sum);
+return 0;
+}
