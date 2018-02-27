a502a52d41ae658feaf0452d32d20509199fbd66
dhivyavsb <31505815+dhivyavsb@users.noreply.github.com>
2018-02-27 11:30:11 +0000

    Create k.c

---
+++ k.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int n, k,i,a[100];
+	scanf("%d\n%d",&n,&k);
+	for(i=1;i<=n;i++)
+	{
+		scanf("%d",&a[i]);
+	if(i=1;i<=n;i++)
+	{
+		k==n;
+	}
+	else
+	{
+		printf("yes");
+	}
+	}
+	return 0;
+	
+}
