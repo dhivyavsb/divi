9c87625a7c6b31e188d7252a4125322da5a2b31a
dhivyavsb <31505815+dhivyavsb@users.noreply.github.com>
2018-03-08 11:02:48 +0000

    Create beginner 66.c

---
+++ beginner 66.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int v,i,c=0;
+scanf("%d",&a);
+for(i=1;i<=v;i++)
+{
+if(v%i==0)
+{
+c++;
+}
+}
+if(c==2)
+{
+printf("prime");
+}
+else
+{
+printf("not a prime");
+}
+return 0;
+}
