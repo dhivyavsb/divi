5b9f719ec21174f46ea5030774c88560cdec5cb0
dhivyavsb <31505815+dhivyavsb@users.noreply.github.com>
2018-03-08 11:17:26 +0000

    Create beginner 68.c

---
+++ beginner 68.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int k;
+	scanf("%d",&k);
+	if(k%7==0)
+	{
+		printf("%d is multiple of 7",k);
+	}
+	else
+	{
+		printf("%d is not multiple of 7",k);
+	}
+	return 0;
+}
