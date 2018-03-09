14552e46267f4779e5f056574b59ecb89c9fe268
dhivyavsb <31505815+dhivyavsb@users.noreply.github.com>
2018-03-09 16:07:26 +0530

    Create beginner 72.c

---
+++ beginner 72.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char s[10],b;
+	char a,e,i,o,u;
+	scanf("%s",s[b]);
+	if((s[b]=='a')||(s[b]=='e')||(s[b]=='i')||(s[b]=='o')||(s[b]=='u'))
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
