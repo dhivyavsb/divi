8be27a8c32db6b10dbfe016079c81d3b33de2121
dhivyavsb <31505815+dhivyavsb@users.noreply.github.com>
2018-03-09 15:44:46 +0530

    Create beginner 71.c

---
+++ beginner 71.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char a[10],b[10];
+	int c;
+	a[10]="lappal";
+	strcpy(a,b);
+	strrev(a);
+	c=strcmp(b,a);
+	if(i==0)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
